c8a6d1a2a1fd6a55d02de79256fe86ba56da06c7
xuinvip <xuin8640@gmail.com>
2021-04-07 21:33:14 +0800

    [Widgets] Finish learn Table widget

--- lib/data/metaData.dart
+++ lib/data/metaData.dart
@@ -5,6 +5,7 @@ import '../widgets/Layout.dart';
 import '../widgets/Wrap.dart';
 import '../widgets/Opacity.dart';
 import '../widgets/PageView.dart';
+import '../widgets/Table.dart';
 
 List<LearnWidget> widgets = [
   LearnWidget('SafeArea', SafeAreaScreen()),
@@ -12,4 +13,5 @@ List<LearnWidget> widgets = [
   LearnWidget('Wrap', WrapScreen()),
   LearnWidget('Opacity', OpacityScreen()),
   LearnWidget('PageView', PageViewScreen()),
+  LearnWidget('Table', TableScreen()),
 ];
---
+++ lib/widgets/Table.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_all_widgets/data/metaData.dart';
+import 'Common.dart';
+
+class TableScreen extends CommonWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('TableScreen'),
+        ),
+        body: Table(
+          // border: TableBorder.all(),
+          border: TableBorder(
+            horizontalInside: BorderSide(width: 2.0, color: Colors.black26),
+            verticalInside: BorderSide(width: 2.0, color: Colors.black26),
+            left: BorderSide(width: 2.0, color: Colors.black26),
+            top: BorderSide(width: 2.0, color: Colors.black26),
+            right: BorderSide(width: 2.0, color: Colors.black26),
+            bottom: BorderSide(width: 2.0, color: Colors.black26),
+          ),
+          columnWidths: {
+            0: IntrinsicColumnWidth(),
+            1: FlexColumnWidth(),
+            2: FixedColumnWidth(128),
+          },
+          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+          children: [
+            TableRow(
+              children: [
+                Container(
+                  height: 32,
+                  color: Colors.green,
+                ),
+                TableCell(
+                  // verticalAlignment: TableCellVerticalAlignment.bottom,
+                  verticalAlignment: TableCellVerticalAlignment.top,
+                  child: Container(
+                    height: 32,
+                    width: 32,
+                    color: Colors.red,
+                  ),
+                ),
+                Container(
+                  height: 128,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+            TableRow(
+              decoration: BoxDecoration(
+                color: Colors.grey,
+              ),
+              children: [
+                Container(
+                  height: 128,
+                  width: 128,
+                  color: Colors.purple,
+                ),
+                Container(
+                  height: 32,
+                  color: Colors.yellow,
+                ),
+                Center(
+                  child: Container(
+                    height: 32,
+                    width: 32,
+                    color: Colors.orange,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ));
+  }
+}
